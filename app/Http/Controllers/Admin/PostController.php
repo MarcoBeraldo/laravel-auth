1e0ffeff5d1d944b5644fce8b3e6e06329fe7e36
MarcoBeraldo <beraldomar@gmail.com>
2022-09-27 20:48:14 +0200

    update function completed

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -41,7 +41,7 @@ class PostController extends Controller
     {
         $data = $request->all();
 
-        $post = new Post;        
+        $post = new Post();        
 
         $post->fill($data);
 
@@ -83,9 +83,21 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $data = $request->all();
+
+        $data['slug'] = Str::slug($data['title'], '-');
+              
+        $post->update($data);
+
+
+
+        
+
+        return redirect()->route('admin.posts.show', $post)
+        ->with('message', "Post modificato con successo")
+        ->with('type', 'success');
     }
 
     /**
