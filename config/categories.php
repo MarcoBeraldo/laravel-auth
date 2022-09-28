7dc44d8ee6dd07c632b33ce38af40d73c7a88b3f
MarcoBeraldo <beraldomar@gmail.com>
2022-09-28 16:19:00 +0200

    created category model and seeder

---
+++ config/categories.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    ['label' => 'HTML', 'color'=> 'danger'],
+    ['label' => 'CSS', 'color'=> 'primary'],
+    ['label' => 'ES6', 'color'=> 'warning'],
+    ['label' => 'Bootstrap', 'color'=> 'info'],
+    ['label' => 'PHP', 'color'=> 'secondary'],
+    ['label' => 'SQL', 'color'=> 'dark'],
+    ['label' => 'Laravel', 'color'=> 'light'],
+    ['label' => 'VueJS', 'color'=> 'success'],
+];
---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Model\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = config('categories');
+
+        foreach ($categories as $category){
+            $new_category = new Category();
+
+            $new_category->label = $category['label'];
+            $new_category->color = $category['color'];
+            $new_category->save();
+        }
+    }
+}
